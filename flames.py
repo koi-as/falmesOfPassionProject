f155251ff79131f92f8eab056cc606033b715cba
koi-as <alextseidensticker@gmail.com>
2025-09-19 12:29:08 -0400

    added relationship logic and label output

--- flames.py
+++ flames.py
@@ -14,30 +14,28 @@ def flames():
     namestr=n1+n2
     for i in namestr: # for the integer count of letters in namestr
         if namestr.count(i) != 1: # if namestr.count(integer length of namestr) is not equal to 1 
-            namestr = namestr.replace(i,"") # replace all repeated letters with blank space. so result of 'google' + 'yahoo' would be 'leyah'
-    print(namestr)
+            namestr=namestr.replace(i,"") # replace all repeated letters with blank space. so result of 'google' + 'yahoo' would be 'leyah'
  
-    # number = len(namestr) % 6
-    # # number to move through FLAMES
-    # global rel
-    # rel= ""
-    # if number == 1:
-    #     rel += "Friends"
-    # elif number == 2:
-    #     rel += "Love"
-    # elif number == 3:
-    #     rel += "Affection"
-    # elif number == 4:
-    #     rel += "Marriage"
-    # elif number == 5:
-    #     rel += "Enemy"
-    # elif number == 0:
-    #     rel += "Siblings"
-    # else:
-    #     pass
+    number=len(namestr)%6 # getting count of new string name and taking remainder (modulus) of division by 6
+    global rel # what is global rel?
+    rel=""
+    if number==1:
+        rel+="Friends"
+    elif number==2:
+        rel+="Love"
+    elif number==3:
+        rel+="Affection"
+    elif number==4:
+        rel+="Marriage"
+    elif number==5:
+        rel+="Enemy"
+    elif number==0:
+        rel+="Siblings"
+    else:
+        pass # what is pass?
    
-    # Label(window,text="According to the Game of Flames the Relation is:").pack()
-    # Label(window,text=rel).pack()
+    Label(win, text="According to the Game of Flames, your relationship is:").pack()
+    Label(win, text=rel).pack()
 
 Label(win, text='Play FLAMES by Koi', font=('Arial', 15), bg='lightgoldenrodyellow').pack()
 
