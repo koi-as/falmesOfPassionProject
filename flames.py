963ff3ed1511daea524871d56fa5225e51ca232a
koi-as <alextseidensticker@gmail.com>
2025-09-19 12:19:43 -0400

    Added string concat function that removes common letters/repeated letters

--- flames.py
+++ flames.py
@@ -9,7 +9,35 @@ name1=StringVar()
 name2=StringVar()
 
 def flames():
-    print('hello')
+    n1=name1.get()
+    n2=name2.get()
+    namestr=n1+n2
+    for i in namestr: # for the integer count of letters in namestr
+        if namestr.count(i) != 1: # if namestr.count(integer length of namestr) is not equal to 1 
+            namestr = namestr.replace(i,"") # replace all repeated letters with blank space. so result of 'google' + 'yahoo' would be 'leyah'
+    print(namestr)
+ 
+    # number = len(namestr) % 6
+    # # number to move through FLAMES
+    # global rel
+    # rel= ""
+    # if number == 1:
+    #     rel += "Friends"
+    # elif number == 2:
+    #     rel += "Love"
+    # elif number == 3:
+    #     rel += "Affection"
+    # elif number == 4:
+    #     rel += "Marriage"
+    # elif number == 5:
+    #     rel += "Enemy"
+    # elif number == 0:
+    #     rel += "Siblings"
+    # else:
+    #     pass
+   
+    # Label(window,text="According to the Game of Flames the Relation is:").pack()
+    # Label(window,text=rel).pack()
 
 Label(win, text='Play FLAMES by Koi', font=('Arial', 15), bg='lightgoldenrodyellow').pack()
 
